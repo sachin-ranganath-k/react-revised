71d8c5d9efb44dd05cb9101b29c31dc1f8cb81e8
ranganathnk <ranganathnkulkarni@gmail.com>
2022-10-01 17:00:14 +0530

    props added

--- react-revisited/src/App.js
+++ react-revisited/src/App.js
@@ -1,9 +1,55 @@
 import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+  const expenses = [
+    {
+      id: "e1",
+      title: "News Paper",
+      amount: 500.00,
+      date: new Date(2022, 7, 14),
+    },
+    { id: "e2", 
+      title: "New TV", 
+      amount: 799.49, 
+      date: new Date(2022, 2, 12) },
+    {
+      id: "e3",
+      title: "Home loan",
+      amount: 294.67,
+      date: new Date(2022, 2, 28),
+    },
+    {
+      id: "e4",
+      title: "Table",
+      amount: 450,
+      date: new Date(2022, 5, 12),
+    },
+  ];
   return (
     <div>
-      <ExpenseItem />
+      <ExpenseItem 
+        expenseTitle={expenses[0].title} 
+        expenseAmount={expenses[0].amount} 
+        expenseDate={expenses[0].date}
+      />
+
+      <ExpenseItem 
+        expenseTitle={expenses[1].title} 
+        expenseAmount={expenses[1].amount} 
+        expenseDate={expenses[1].date}
+      />
+
+      <ExpenseItem 
+        expenseTitle={expenses[2].title} 
+        expenseAmount={expenses[2].amount} 
+        expenseDate={expenses[2].date}
+      />
+
+      <ExpenseItem 
+        expenseTitle={expenses[3].title} 
+        expenseAmount={expenses[3].amount} 
+        expenseDate={expenses[3].date}
+      />
     </div>
   );
 }
--- react-revisited/src/components/ExpenseItem.jsx
+++ react-revisited/src/components/ExpenseItem.jsx
@@ -1,18 +1,18 @@
 import React from "react";
 import "./ExpenseItem.css";
 
-const ExpenseItem = () => {
+const ExpenseItem = (propsFromApp) => {
 
-    const expenseDate=new Date(2022, 9, 25);
-    const expenseItem='Home loan';
-    const expenseAmount= 20000
+    // const expenseDate=new Date(2022, 9, 25);
+    // const expenseItem='Home loan';
+    // const expenseAmount= 20000
 
   return (
     <div className="expense-item">
-      <div>{expenseDate.toISOString()}</div>
+      <div>{propsFromApp.expenseDate.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>{expenseItem}</h2>
-        <div className="expense-item__price">{expenseAmount}/-</div>
+        <h2>{propsFromApp.expenseTitle}</h2>
+        <div className="expense-item__price">{propsFromApp.expenseAmount}/-</div>
       </div>
     </div>
   );
