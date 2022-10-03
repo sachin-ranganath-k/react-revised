9fd507379e4fad053bf093e104d5df61428ed4c9
ranganathnk <ranganathnkulkarni@gmail.com>
2022-10-03 18:52:27 +0530

    state (useState())

--- react-revisited/src/components/ExpenseItem.jsx
+++ react-revisited/src/components/ExpenseItem.jsx
@@ -1,20 +1,24 @@
-import React from "react";
+import React, {useState} from "react";
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 //import CardContainer from "./CardContainer";
 
 const ExpenseItem = (propsFromApp) => {
+
+  const [expenseTitle, setExpenseTitle]=useState(propsFromApp.expenseTitle);
+
   return (
     // <CardContainer className="expense-item">   This can be used (props.children)
     <div className="expense-item">
       {/* Passing date prop to ExpenseDate from here */}
       <ExpenseDate date={propsFromApp.expenseDate} />
-      <div className="expense-item__description">
-        <h2>{propsFromApp.expenseTitle}</h2>
+      <div className="expense-item__description"> 
+        <h2>{expenseTitle}</h2>
         <div className="expense-item__price">
           {propsFromApp.expenseAmount}/-
         </div>
       </div>
+      <button onClick={()=>setExpenseTitle('Computer')}>Change</button>
     </div>
     // </CardContainer>
   );
